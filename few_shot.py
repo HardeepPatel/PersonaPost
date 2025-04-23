52cc0e2d1f3b7822519ab561cfd42617aca3536e
HardeepPatel <85514238+HardeepPatel@users.noreply.github.com>
2025-04-23 11:44:17 -0700

    Update few_shot.py

--- few_shot.py
+++ few_shot.py
@@ -1,27 +1,33 @@
+from pymongo import MongoClient
 import pandas as pd
-import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class FewShotPosts:
-    def __init__(self, file_path="data/processed_posts.json"):
+    def __init__(self, db_name="genai", collection_name="posts"):
         self.df = None
         self.unique_tags = None
-        self.load_posts(file_path)
+        self.load_posts(db_name, collection_name)
 
-    def load_posts(self, file_path):
-        with open(file_path, encoding="utf-8") as f:
-            posts = json.load(f)
-            self.df = pd.json_normalize(posts)
-            self.df['length'] = self.df['line_count'].apply(self.categorize_length)
-            # collect unique tags
-            all_tags = self.df['tags'].apply(lambda x: x).sum()
-            self.unique_tags = list(set(all_tags))
+    def load_posts(self, db_name, collection_name):
+        mongo_uri = os.getenv("MONGO_URI")  # Add MONGO_URI in your .env
+        client = MongoClient(mongo_uri)
+        db = client[db_name]
+        collection = db[collection_name]
+        
+        posts = list(collection.find({}, {"_id": 0}))  # Exclude _id from documents
+        self.df = pd.json_normalize(posts)
+        self.df['length'] = self.df['line_count'].apply(self.categorize_length)
+        all_tags = self.df['tags'].apply(lambda x: x).sum()
+        self.unique_tags = list(set(all_tags))
 
     def get_filtered_posts(self, length, language, tag):
         df_filtered = self.df[
-            (self.df['tags'].apply(lambda tags: tag in tags)) &  # Tags contain 'Influencer'
-            (self.df['language'] == language) &  # Language is 'English'
-            (self.df['length'] == length)  # Line count is less than 5
+            (self.df['tags'].apply(lambda tags: tag in tags)) &
+            (self.df['language'] == language) &
+            (self.df['length'] == length)
         ]
         return df_filtered.to_dict(orient='records')
 
@@ -39,6 +45,5 @@ class FewShotPosts:
 
 if __name__ == "__main__":
     fs = FewShotPosts()
-    # print(fs.get_tags())
-    posts = fs.get_filtered_posts("Medium","Hinglish","Job Search")
+    posts = fs.get_filtered_posts("Medium", "Hinglish", "Job Search")
     print(posts)
